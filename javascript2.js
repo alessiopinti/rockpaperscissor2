754b5beb43010b03b32f96ea28297a8c81a9e6e1
Alessio Pinti <alessiopinti@hotmail.com>
2023-12-27 21:06:14 +0100

    getComputerChoice function added with working code

---
+++ javascript2.js
@@ -0,0 +1,5 @@
+function getComputerChoice(){
+  let playerSelection = prompt("rock, paper or scissors?");
+  let selection = ["rock", "paper", "scissors"];
+  let item = selection[Math.floor(Math.random()*selection.length)];
+}
