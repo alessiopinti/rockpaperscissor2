6e3b21932f39be6f04a371ef5749597e8a563edd
Alessio Pinti <alessiopinti@hotmail.com>
2023-12-29 15:40:06 +0100

    updated getComputerChoice function and added new function called playRound and added code

--- javascript2.js
+++ javascript2.js
@@ -1,5 +1,25 @@
+let playerSelection = prompt("rock, paper or scissors?");
+computerSelection = getComputerChoice();
+
 function getComputerChoice(){
-  let playerSelection = prompt("rock, paper or scissors?");
   let selection = ["rock", "paper", "scissors"];
-  let item = selection[Math.floor(Math.random()*selection.length)];
+  let computerSelection = selection[Math.floor(Math.random()*selection.length)];
+  return computerSelection
 }
+
+function playRound (playerSelection, computerSelection) {
+
+  if (computerSelection === playerSelection ){
+    console.log("It's a tie.");
+  }
+  
+  if (computerSelection === "rock" && playerSelection === "scissors" || computerSelection === "paper" && playerSelection === "rock" || computerSelection === "scissors" && playerSelection === "paper"){
+    console.log("You lose.");
+  }
+
+  if (computerSelection === "rock" && playerSelection === "paper" || computerSelection === "paper" && playerSelection === "scissors" || computerSelection === "scissors" && playerSelection === "rock"){
+    console.log("You win.");
+  }
+}
+
+playRound(playerSelection, computerSelection);
